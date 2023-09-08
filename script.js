d2f8990dfc52b185cb26df82b322554bfcd48d63
mertokerto <meteor6114@gmail.com>
2023-09-08 21:19:18 +0300

    added eraser button and added a mod so that coloring mode is switchable instead of being mouseover

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let color = "black";
+let click = true;
 
 function populateBoard(size) {
      let board = document.querySelector(".board");
@@ -24,10 +25,12 @@ function changeSize(input) {
 }
 
 function colorSquare() {
-     if (color === "random") {
-          this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-     } else {
-          this.style.backgroundColor = color;
+     if (click) {
+          if (color === "random") {
+               this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+          } else {
+               this.style.backgroundColor = color;
+          }
      }
 }
 
@@ -40,3 +43,14 @@ function resetBoard() {
      let squares = board.querySelectorAll("div");
      squares.forEach((div) => (div.style.backgroundColor = "white"));
 }
+
+document.querySelector("body").addEventListener("click", (e) => {
+     if (e.target.tagName != "BUTTON") {
+          click = !click;
+          if (click) {
+               document.querySelector(".mode").textContent = "Mode: Coloring";
+          } else {
+               document.querySelector(".mode").textContent = "Mode: Not Coloring";
+          }
+     }
+});
